385aefffbc81f375154ffd5559c3436a03955d13
Nicholas Manha <nickrmanha@gmail.com>
2024-11-11 22:12:11 -0800

    implemented Server listening loop

--- BSS/src/Server.java
+++ BSS/src/Server.java
@@ -1,9 +1,65 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ServerSocket server = null;
+		try {
+
+			// server is listening on port 1234
+			server = new ServerSocket(1234);
+			server.setReuseAddress(true);
+
+			// running infinite loop for getting
+			// client request
+			while (true) {
+
+				// socket object to receive incoming client
+				// requests
+				Socket client = server.accept();
+
+				// Displaying that new client is connected
+				// to server
+				System.out.println("New client connected"
+								+ client.getInetAddress()
+										.getHostAddress());
+
+				// create a new thread object
+				ClientHandler clientSock
+					= new ClientHandler(client);
+
+				// This thread will handle the client
+				// separately
+				new Thread(clientSock).start();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (server != null) {
+				try {
+					server.close();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 	}
+	private static class ClientHandler implements Runnable {
+		private final Socket clientSocket;
+		public ClientHandler(Socket socket)
+		{
+			this.clientSocket = socket;
+		}
+		public void run() {
+			
+		}
+	}
+	
 
 }
