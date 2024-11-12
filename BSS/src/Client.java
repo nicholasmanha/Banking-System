b4af143d71e9fca67d19a59fe04ce42caed44fdc
Nicholas Manha <nickrmanha@gmail.com>
2024-11-11 22:26:58 -0800

    added two way connection for client

--- BSS/src/Client.java
+++ BSS/src/Client.java
@@ -1,8 +1,31 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
 
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try (Socket socket = new Socket("ip goes here", 1234)) {
+			// output, to send TO the server
+			OutputStream outputStream = socket.getOutputStream();
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+			
+			// input, to recieve FROM the server
+			InputStream inputStream = socket.getInputStream();
+			ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+	        
+	        System.out.println("Closing socket");
+	        socket.close();
+	        
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
