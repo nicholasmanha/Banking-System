41f30aefe698f970b18bf85a7be2f857e66fe469
Nicholas Manha <nickrmanha@gmail.com>
2024-11-11 22:13:30 -0800

    added two way connection in clientHandler w/ request object

--- BSS/src/Server.java
+++ BSS/src/Server.java
@@ -1,6 +1,13 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 
 public class Server {
 
@@ -57,7 +64,50 @@ public class Server {
 			this.clientSocket = socket;
 		}
 		public void run() {
-			
+			PrintWriter out = null;
+			BufferedReader in = null;
+			try {
+					
+				// get the outputstream of client
+				OutputStream outputStream = clientSocket.getOutputStream();
+				ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+				// get the inputstream of client
+				InputStream inputStream = clientSocket.getInputStream();
+				ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+				
+				
+				try {
+					
+					Request message = (Request) objectInputStream.readObject();
+			        
+			        System.out.println("Closing socket " + clientSocket.getRemoteSocketAddress());
+			        clientSocket.close();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+
+				
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (out != null) {
+						out.close();
+					}
+					if (in != null) {
+						in.close();
+						clientSocket.close();
+					}
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	
