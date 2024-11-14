d6b8f268faf7a89c7fd54bc50cc04efb66292797
klam224 <kylelam242@gmail.com>
2024-11-13 23:19:32 -0800

    Did the find methods for customer and teller

--- BSS/src/bss/Bank.java
+++ BSS/src/bss/Bank.java
@@ -1,6 +1,7 @@
 package bss;
 import java.util.ArrayList;
 
+
 public class Bank {
 
 	private ArrayList<Account> accounts;
@@ -14,6 +15,73 @@ public class Bank {
 		this.tellers = new ArrayList<>();
 	}
 	
+	// add an account to the bank
+	public void addAccount(Account account) {
+		accounts.add(account);
+	}
 	
+	// add an customer to the bank
+	public void addAccount(Customer customer) {
+		customers.add(customer);
+	}
+	
+	// add an teller to the bank
+	public void addAccount(Teller teller) {
+		tellers.add(teller);
+	}
+	
+	// find an account by account ID
+	public Account findAccount(int account_id){
+		
+		for (Account account : accounts) 
+		{
+	        if (account.getAccountID() == account_id) 
+	        {
+	            return account;
+	        }
+	    }
+		
+	    return null;
+	}
+	
+	// find a customer by customer ID
+	public Customer findCustomer(int customerID) {
+
+		for (Customer customer : customers) 
+		{
+			// Assuming each customer has a unique ID and a getID() method
+            if (customer.getID() == customerID) 
+            {
+                return customer;
+            }
+        }
+		
+        return null;
+    }
+	
+	// Find a teller by teller ID
+	public Teller findTeller(int teller_id) {
+        
+		for (Teller teller : tellers) 
+		{
+			// assuming each teller has a unique ID and a getID() method
+            if (teller.getID() == tellerID) 
+            {
+                return teller;
+            }
+        }
+		
+        return null;
+    }
+	
+	// save bank data to a file
+	public void saveData() {
+		
+	}
+	
+	// load bank data from a file
+	public void loadData() {
+		
+	}
 
 }
