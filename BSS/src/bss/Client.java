ce76147d4451de2862e33021e445f4ff8e9593ae
Nicholas Manha <nickrmanha@gmail.com>
2024-11-20 19:56:26 -0800

    client can now process recieved response

--- BSS/src/bss/Client.java
+++ BSS/src/bss/Client.java
@@ -3,7 +3,6 @@ package bss;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -17,43 +16,64 @@ import enums.*;
 
 public class Client {
 	private static OutHandler outHandler;
-	
+	private static boolean isLoggedIn = true;
+
 	public Client(OutHandler outHandler) {
 		Client.outHandler = outHandler;
 	}
+
 	public static void main(String[] args) {
 		try (Socket socket = new Socket("localhost", 1234)) {
-			
+
 			// output, to send TO the server
 			OutputStream outputStream = socket.getOutputStream();
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-			
+
 			InputStream inputStream = socket.getInputStream();
 			ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
 
-            InputHandler inputHandler = new InputHandler(objectInputStream);
-            Thread inputThread = new Thread(inputHandler);
-            inputThread.start();
-            
-            OutHandler outHandler = new OutHandler(objectOutputStream);
+			InputHandler inputHandler = new InputHandler(objectInputStream);
+			Thread inputThread = new Thread(inputHandler);
+			inputThread.start();
+
+			OutHandler outHandler = new OutHandler(objectOutputStream);
 
 			Thread outputThread = new Thread(outHandler);
 			outputThread.start();
-			
+
 			Client client = new Client(outHandler);
-			
+
 			BSSConsoleUI UI = new BSSConsoleUI(client);
 			UI.processCommands();
-			
+			while (isLoggedIn) {
+				List<Request> req = inputHandler.getNextRequest();
+				if (req != null) {
+
+					System.out.println("Received: " + req);
+					processResponse(req);
+				}
+
+				Thread.sleep(1000);
+			}
 			outputThread.join();
-			System.out.println("Closing socket");
 			
+			System.out.println("Closing socket");
+
 			socket.close();
 
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	private static void processResponse(List<Request> req) {
+		for (Request request : req) {
+			if (request.getType() == RequestType.LOGIN && request.getStatus() == Status.SUCCESS) {
+				System.out.println("login successful");
+
+			}
+		}
 	}
 
 	public void createLoginRequest(String username, String password) {
@@ -63,15 +83,13 @@ public class Client {
 		Request loginRequest = new Request(userAndPass, RequestType.LOGIN, Status.REQUEST);
 		List<Request> requests = new ArrayList<Request>();
 		requests.add(loginRequest);
-		
 
-		
 		outHandler.enqueueRequest(requests);
 	}
 
 	private static class InputHandler implements Runnable {
 		private final ObjectInputStream inputStream;
-		private final ConcurrentLinkedQueue<List<Object>> requestQueue;
+		private final ConcurrentLinkedQueue<List<Request>> requestQueue;
 		private boolean running = true;
 
 		public InputHandler(ObjectInputStream in) {
@@ -79,15 +97,12 @@ public class Client {
 			this.requestQueue = new ConcurrentLinkedQueue<>();
 		}
 
-		public void enqueueRequest(List<Object> requests) {
-			requestQueue.add(requests);
-		}
 
 		public void run() {
 			while (running) {
 				try {
-					List<Object> requests = (List<Object>) inputStream.readObject();
-					if( requests != null) {
+					List<Request> requests = (List<Request>) inputStream.readObject();
+					if (requests != null) {
 						requestQueue.add(requests);
 					}
 				} catch (IOException | ClassNotFoundException e) {
@@ -104,14 +119,16 @@ public class Client {
 
 			}
 		}
+
 		public void stop() {
-	        running = false;
-	    }
-		public Object getNextMessage() {
-	        return requestQueue.poll();
-	    }
+			running = false;
+		}
+
+		public List<Request> getNextRequest() {
+			return requestQueue.poll();
+		}
 	}
-	
+
 	private static class OutHandler implements Runnable {
 		private final ObjectOutputStream outputStream;
 		private final ConcurrentLinkedQueue<List<Request>> requestQueue;
@@ -138,7 +155,7 @@ public class Client {
 					}
 				}
 				try {
-					
+
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
--- BSS/src/bss/Server.java
+++ BSS/src/bss/Server.java
@@ -13,12 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-
 import enums.*;
 import requests.Request;
 
 public class Server {
+	
 
+	
 	public static void main(String[] args) {
 		ServerSocket server = null;
 		try {
@@ -58,19 +59,11 @@ public class Server {
 		}
 
 	}
+
 	
-	private static void processRequest(List<Request> req) {
-		for(Request request : req) {
-			if(request.getType() == RequestType.LOGIN) {
-				System.out.println("login request recieved");
-				String username = request.getTexts().get(0);
-				String password = request.getTexts().get(1);
-				System.out.println(username + ", " + password);
-			}
-		}
-	}
-	
+
 	private static class ClientHandler implements Runnable {
+		private static OutHandler outHandler;
 		private final Socket clientSocket;
 		Bank bank;
 
@@ -88,72 +81,106 @@ public class Server {
 
 			try {
 				ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
-				
+
 				ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
 
-	            InputHandler inputHandler = new InputHandler(objectInputStream);
-	            Thread inputThread = new Thread(inputHandler);
-	            inputThread.start();
-	            
-	            OutHandler outHandler = new OutHandler(objectOutputStream);
-	            Thread outputThread = new Thread(outHandler);
+				InputHandler inputHandler = new InputHandler(objectInputStream);
+				Thread inputThread = new Thread(inputHandler);
+				inputThread.start();
+
+				OutHandler outHandler = new OutHandler(objectOutputStream);
+				ClientHandler.outHandler = outHandler;
+				Thread outputThread = new Thread(outHandler);
 				outputThread.start();
-	            
-	            while (true) {
-	                List<Request> req = inputHandler.getNextRequest();
-	                if (req != null) {
-	                	
-	                    System.out.println("Received: " + req);
-	                    processRequest(req);
-	                }
-	                
-	                Thread.sleep(1000);
-	            }
+
+				while (true) {
+					List<Request> req = inputHandler.getNextRequest();
+					if (req != null) {
+
+						System.out.println("Received: " + req);
+						processRequest(req);
+					}
+
+					Thread.sleep(1000);
+				}
 
 			} catch (IOException | InterruptedException e) {
 				e.printStackTrace();
-			
+
+			}
 		}
-	}
-
-		private static class OutHandler implements Runnable {
-			private final ObjectOutputStream outputStream;
-			private final ConcurrentLinkedQueue<List<Object>> requestQueue;
-			private boolean running = true;
-
-			public OutHandler(ObjectOutputStream out) {
-				this.outputStream = out;
-				this.requestQueue = new ConcurrentLinkedQueue<>();
-			}
-
-			public void enqueueRequest(List<Object> requests) {
-				requestQueue.add(requests);
-			}
-
-			public void run() {
-				while (running) {
-					List<Object> requests = requestQueue.poll();
-					if (requests != null) {
-						try {
-
-							outputStream.writeObject(requests);
-							outputStream.flush();
-						} catch (IOException e) {
-							running = false;
-						}
-					}
-
-					try {
-						
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-
+		private static void processRequest(List<Request> req) {
+			for (Request request : req) {
+				if (request.getType() == RequestType.LOGIN && request.getStatus() == Status.REQUEST) {
+					System.out.println("login request recieved");
+					String username = request.getTexts().get(0);
+					String password = request.getTexts().get(1);
+					System.out.println(username + ", " + password);
+					List<Request> responses = new ArrayList<Request>();
+					responses.add(new Request(Requester.USER, RequestType.LOGIN, Status.SUCCESS));
+					outHandler.enqueueRequest(responses);
 				}
 			}
 		}
+
 		
+
+		private static UserType determineUserType(Bank bank, int userID) {
+
+			Teller teller;
+			Account acc;
+			acc = bank.findAccount(userID);
+			if (acc == null) {
+				teller = bank.findTeller(userID);
+				if (teller == null) {
+					System.out.println("account undefined");
+					return UserType.Undefined;
+				}
+				return UserType.Teller;
+			}
+			return UserType.Customer;
+		}
+
+	}
+	private static class OutHandler implements Runnable {
+		private final ObjectOutputStream outputStream;
+		private final ConcurrentLinkedQueue<List<Request>> requestQueue;
+		private boolean running = true;
+
+		public OutHandler(ObjectOutputStream out) {
+			this.outputStream = out;
+			this.requestQueue = new ConcurrentLinkedQueue<>();
+		}
+
+		public void enqueueRequest(List<Request> requests) {
+			requestQueue.add(requests);
+		}
+
+		public void run() {
+			while (running) {
+				List<Request> requests = requestQueue.poll();
+				if (requests != null) {
+					try {
+						
+						outputStream.writeObject(requests);
+						outputStream.flush();
+						System.out.println("sent message");
+					} catch (IOException e) {
+						running = false;
+					}
+				}
+
+				try {
+
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
 	private static class InputHandler implements Runnable {
 		private final ObjectInputStream inputStream;
 		private final ConcurrentLinkedQueue<List<Request>> requestQueue;
@@ -164,13 +191,11 @@ public class Server {
 			this.requestQueue = new ConcurrentLinkedQueue<>();
 		}
 
-
-
 		public void run() {
 			while (running) {
 				try {
 					List<Request> requests = (List<Request>) inputStream.readObject();
-					if( requests != null) {
+					if (requests != null) {
 						requestQueue.add(requests);
 					}
 				} catch (IOException | ClassNotFoundException e) {
@@ -187,29 +212,13 @@ public class Server {
 
 			}
 		}
+
 		public void stop() {
-	        running = false;
-	    }
-		public List<Request> getNextRequest() {
-	        return requestQueue.poll();
-	    }
-	}
-
-	private static UserType determineUserType(Bank bank, int userID) {
-
-		Teller teller;
-		Account acc;
-		acc = bank.findAccount(userID);
-		if (acc == null) {
-			teller = bank.findTeller(userID);
-			if (teller == null) {
-				System.out.println("account undefined");
-				return UserType.Undefined;
-			}
-			return UserType.Teller;
+			running = false;
 		}
-		return UserType.Customer;
-	}
 
-}
+		public List<Request> getNextRequest() {
+			return requestQueue.poll();
+		}
+	}
 }
