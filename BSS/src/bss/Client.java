0abc4e3e19afb40db8e1a3b3c0f84501ad7898df
Nicholas Manha <nickrmanha@gmail.com>
2024-11-22 00:48:19 -0800

    removed inputHandler on client and server

--- BSS/src/bss/Client.java
+++ BSS/src/bss/Client.java
@@ -183,49 +183,5 @@ public class Client {
 		outputHandler.enqueueRequest(requests);
 	}
 
-	/*
-	 * INPUT HANDLER
-	 */
-	private static class InputHandler implements Runnable {
-		private final ObjectInputStream inputStream;
-		private final ConcurrentLinkedQueue<List<Request>> requestQueue;
-		private boolean running = true;
-
-		public InputHandler(ObjectInputStream in) {
-			this.inputStream = in;
-			this.requestQueue = new ConcurrentLinkedQueue<>();
-		}
-
-		public void run() {
-			// put responses in queue for processing (which is done in the main method)
-			while (running) {
-				try {
-					List<Request> requests = (List<Request>) inputStream.readObject();
-					if (requests != null) {
-						requestQueue.add(requests);
-					}
-				} catch (IOException | ClassNotFoundException e) {
-					e.printStackTrace();
-					running = false;
-				}
-
-				try {
-
-					Thread.sleep(200);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-
-			}
-		}
-
-		public void stop() {
-			running = false;
-		}
-
-		public List<Request> getNextRequest() {
-			return requestQueue.poll();
-		}
-	}
-
+	
 }
