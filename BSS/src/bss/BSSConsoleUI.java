cca10a834c829b79e09b4051c761b4f608306201
Nicholas Manha <nickrmanha@gmail.com>
2024-11-21 16:39:40 -0800

    added withdraw method in ConsoleUI

--- BSS/src/bss/BSSConsoleUI.java
+++ BSS/src/bss/BSSConsoleUI.java
@@ -37,7 +37,7 @@ public class BSSConsoleUI implements Runnable{
             System.out.print(".");
 		}
 		System.out.println();
-		String[] commands = { "Deposit", "Logout" };
+		String[] commands = { "Deposit", "Withdraw", "Transfer", "Logout" };
 
 		int choice;
 
@@ -53,6 +53,12 @@ public class BSSConsoleUI implements Runnable{
 					doDeposit();
 					break;
 				case 1:
+					doWithdraw();
+					break;
+				case 2:
+					doTransfer();
+					break;
+				case 3:
 					doLogout();
 					break;
 				default:
@@ -71,6 +77,14 @@ public class BSSConsoleUI implements Runnable{
 		System.out.println("Enter amount");
 		Double amount = scan.nextDouble();
 		client.createDepositRequest(amount);
+	}
+	private void doWithdraw() {
+		System.out.println("Enter amount");
+		Double amount = scan.nextDouble();
+		client.createWithdrawRequest(amount);
+	}
+	private void doTransfer() {
+		
 	}
 	private void doLogout() {
 		System.out.println("cya");
