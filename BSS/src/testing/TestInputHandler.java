4bf02fa68c5c778b9695b2b9e32c41ecf1ec2424
Nicholas Manha <nickrmanha@gmail.com>
2024-12-03 16:46:16 -0800

    made TestInputHandler

---
+++ BSS/src/testing/TestInputHandler.java
@@ -0,0 +1,9 @@
+package testing;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestInputHandler {
+
+}
